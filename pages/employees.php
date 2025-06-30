fcd19e5600fa0888ed3d102b1da2d4b1ae047cd6
Andrianitsoa Mahatoky <andrian.mahatoky@gmail.com>
2025-06-30 12:46:21 +0300

    sauvegarde

--- pages/employees.php
+++ pages/employees.php
@@ -9,42 +9,64 @@ $suivant = $current + 10;
 $employees = getEmpDept($dept_no, $current, 10);
 ?>
 <section class="container">
-    <article class="info-dept">
+    <section class="info-dept">
         <h1><?= $infoDept["dept_name"] ?></h1>
-    </article>
-    <article class="container">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Employees name</th>
-                    <th scope="col">Gender</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($employees as $employee) { ?>
+    </section>
+    <section class="row gap-lg-3">
+        <article class="card col-12 col-md-7">
+            <table class="table">
+                <thead>
                     <tr>
-                        <td>
-                            <a href="modele.php?page=fiche.php&&emp_no=
-                            <?= $employee["emp_no"] ?>">
-                                <?= $employee["first_name"] ?>
-                                <?= $employee["last_name"] ?>
-                            </a>
-                        </td>
-                        <td><?= $employee["gender"] ?></td>
+                        <th scope="col">Employees name</th>
+                        <th scope="col">Gender</th>
                     </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-        <nav>
-            <ul>
-                <?php if ($precedent >= 0) { ?>
-                    <li><a href="modele.php?page=employees.php&dept_no=<?= $dept_no ?>&debutSelect=<?= $precedent ?>">Precedent</a></li>
-                <?php } ?>
-                <li><a href="modele.php?page=employees.php&dept_no=<?= $dept_no ?>&debutSelect=<?= $suivant ?>">suivant</a></li>
-            </ul>
-        </nav>
-    </article>
+                </thead>
+                <tbody>
+                    <?php foreach ($employees as $employee) { ?>
+                        <tr>
+                            <td>
+                                <a href="modele.php?page=fiche.php&&emp_no=
+                            <?= $employee["emp_no"] ?>">
+                                    <?= $employee["first_name"] ?>
+                                    <?= $employee["last_name"] ?>
+                                </a>
+                            </td>
+                            <td><?= $employee["gender"] ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+            <div class="card-footer">
+                <nav>
+                    <ul>
+                        <?php if ($precedent >= 0) { ?>
+                            <li><a href="modele.php?page=employees.php&dept_no=<?= $dept_no ?>&debutSelect=<?= $precedent ?>">Precedent</a></li>
+                        <?php } ?>
+                        <li><a href="modele.php?page=employees.php&dept_no=<?= $dept_no ?>&debutSelect=<?= $suivant ?>">suivant</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        </article>
+        <article class=" card other-info col-12 col-md-4 d-flex flex-column gap-4 p-3">
+            <div class="card text-bg-primary mb-3  " style="max-width: 18rem;">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Primary card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div class="card text-bg-primary mb-3 " style="max-width: 18rem;">
+                <div class="card-header">Header</div>
+                <div class="card-body">
+                    <h5 class="card-title">Primary card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </article>
+    </section>
 </section>
+
 </body>
 
 </html>
--- pages/modele.php
+++ pages/modele.php
@@ -7,40 +7,55 @@ include("../includes/fonctions.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/fontawesome/css/all.css">
 </head>
 
-<body class="d-flex gap-3">
+<body class="row">
     <!-- barre de navigation lateral -->
-    <aside class="col-3 col-md-2 rounded shadow-lg p-5 text-white">
-        <ul class="nav flex-column text-center">
-            <li class="nav-item ">
-                <a class="nav-link active" aria-current="page" href="modele.php?page=departments.php">Department</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="modele.php?page=search_emp.php">search</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-            </li>
-        </ul>
-    </aside>
-    <main class="col- col-md-10">
-        <section>
-            <?php
-            if ($_SERVER["REQUEST_METHOD"] === "GET") {
-                include($_GET["page"]);
-            } else {
-                echo "auccune page chargé";
-            }
-            ?>
-        </section>
+    <header class="row position-fix">
+        <a data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">
+            <i class="fas fa-bars fa-2x"></i>
+        </a>
+    </header>
+    <!-- teste css -->
+
+
+    <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasScrollingLabel">navigation</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="nav flex-column text-center">
+                <li class="nav-item ">
+                    <a class="nav-link active" aria-current="page" href="modele.php?page=departments.php">Department</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="modele.php?page=search_emp.php">search</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <main>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] === "GET") {
+            include($_GET["page"]);
+        } else {
+            echo "auccune page chargé";
+        }
+        ?>
+
 
     </main>
+    <script src="../assets/bootstrap/js/bootstrap.bundle.js"></script>
 </body>
 
 </html>
